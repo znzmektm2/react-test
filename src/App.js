2ef83b562645c1b31e20369a73f166a20714ab69
znzmektm2 <tingcber2@naver.com>
2020-01-23 17:52:50 +0900

    기능: Material UI를 적용하여 고객 테이블 디자인하기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import Customer from './components/Customer';
 import './App.css';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles} from '@material-ui/core/styles';
 
 const custmoers = [{
   'id' : 1,
@@ -32,21 +38,21 @@ class App extends Component {
   render() {
     return (
       <div>
-        {
-          custmoers.map(c => {
-            return (
-              <Customer
-                key={c.id}
-                id={c.id}
-                image={c.image}
-                name={c.name}
-                birthday={c.birthday}
-                gender={c.gender}
-                job={c.job}
-              />
-            )
-          })
-        }
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {custmoers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> ); })}
+            </TableBody>
+          </Table>
       </div>      
     );
   }
