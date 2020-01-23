1371971dae2e1179e6982eed3d58ddef2874f76a
znzmektm2 <tingcber2@naver.com>
2020-01-23 16:19:46 +0900

    기능: 고객 컴포넌트 구조화하기

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer';
 import './App.css';
+
+const custmoers = [{
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/1',
+  'name' : '홍길동',
+  'birthday' : '111111',
+  'gender' : '남자',
+  'job' : '대학생' 
+},
+{
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '전애란',
+  'birthday' : '880510',
+  'gender' : '여자',
+  'job' : '개발자' 
+},
+{
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '이순신',
+  'birthday' : '921215',
+  'gender' : '남자',
+  'job' : '대학생' 
+},
+]
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello React~!
-          </p>
-        </header>
-      </div>
+      <div>
+        {
+          custmoers.map(c => {
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            )
+          })
+        }
+      </div>      
     );
   }
 }
